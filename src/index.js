e99b4dcefcf02e39db8b3680fa859feb30c5ce19
Matthew Masiello <home@Matthews-MBP.fios-router.home>
2019-03-18 22:39:48 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const createTaskForm = document.getElementById("create-task-form");
+  const taskUl = document.getElementById("tasks");
+  const newTaskInput = document.getElementById("new-task-description");
+
+  function addTask(text){
+    let listItem = document.createElement("li");
+    listItem.setAttribute("id", `${text}`);
+    listItem.innerText = text;
+    listItem.innerHTML += "<button>X</button>";
+    taskUl.append(listItem);
+  };
+
+  createTaskForm.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if(e.target.type === "submit"){
+      addTask(newTaskInput.value);
+    }
+  })
+
+  taskUl.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON") {
+      taskUl.removeChild(e.target.parentNode);
+    }
+  });
+
 });
